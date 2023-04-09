3e6908c2d612db9b309a43caa4f296846f9b2082
SFRL <sebastian.lobbers@googlemail.com>
2023-04-09 18:58:19 +0200

    add people

---
+++ src/content/people/member2.md
@@ -0,0 +1,6 @@
+---
+name: "Max Mustermann"
+role: "PhD Student"
+topic: "Machine learning for music"
+image: "./defaultprofile.png"
+---
--- src/pages/people.js
+++ src/pages/people.js
@@ -1,12 +1,48 @@
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import Layout from "../components/layout";
+import PeopleCard from "../components/peopleCard";
 
 
 const People = () => {
+    const data = useStaticQuery(graphql`
+      {
+        allMarkdownRemark(
+          filter: { fields: { category: { eq: "people" } } }
+          sort: { frontmatter: { role: DESC } }
+        ) {
+          nodes {
+            frontmatter {
+              image {
+                childImageSharp {
+                  gatsbyImageData(layout: CONSTRAINED)
+                }
+              }
+              name
+              role
+              topic
+            }
+            id
+          }
+        }
+      }
+    `);
     return (
         <Layout name="People">
-            <h1>People</h1>
+            <section className="section">
+                <div className="columns is-multiline">
+                    {data.allMarkdownRemark.nodes.map((peopleentry) => (
+                        <div className="column is-one-quarter is-one-third-tabled is-full-mobile is-flex" key={peopleentry.id}>
+                            <PeopleCard
+                                name={peopleentry.frontmatter.name}
+                                role={peopleentry.frontmatter.role}
+                                topic={peopleentry.frontmatter.topic}
+                                image={peopleentry.frontmatter.image.childImageSharp.gatsbyImageData}
+                            />
+                        </div>
+                    ))}
+                </div>
+            </section>
         </Layout>
     );
 }
