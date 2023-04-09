b2b93195ae5d1e54a77700c889945578c88023bf
SFRL <sebastian.lobbers@googlemail.com>
2023-04-09 17:22:13 +0200

    rework navbar and move footer to bottom

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,11 +23,11 @@ const Layout = (props) => {
 
     const data = useStaticQuery(graphql`
     {
-    c4dmlogo: file(absolutePath: {regex: "/images/.*icon.*/"}, extension: {eq: "png"}) {
+    c4dmlogo: file(absolutePath: {regex: "/images/.*c4dm.*/"}, extension: {eq: "png"}) {
       id
       publicURL
       childImageSharp {
-        gatsbyImageData(layout: FIXED, height: 50)
+        gatsbyImageData(layout: FIXED, height: 30)
         }
       }
     qmullogo: file(absolutePath: {regex: "/images/.*QMUL_black.*/"}, extension: {eq: "png"}) {
@@ -41,8 +41,8 @@ const Layout = (props) => {
 
     const [showMobileMenu,setShowMobileMenu] = useState(false);
 
-    const navBarTop = (
-      <nav className="navbar" role="navigation" aria-label="main navigation">
+    const navBar = (
+      <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
           <Link to="/">
             <GatsbyImage
@@ -53,53 +53,32 @@ const Layout = (props) => {
           </Link>
           <a
             role="button"
-            className={`navbar-burger ${showMobileMenu?"is-active":undefined}`}
+            className={`navbar-burger ${
+              showMobileMenu ? "is-active" : undefined
+            }`}
             aria-label="menu"
             aria-expanded="false"
             data-target="navbarBasicExample"
-            onClick={()=>setShowMobileMenu(!showMobileMenu)}
+            onClick={() => setShowMobileMenu(!showMobileMenu)}
           >
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
           </a>
         </div>
-        <div className={`navbar-end ${!showMobileMenu?"is-hidden-touch":undefined}`}>
+        <div
+          className={`navbar-end ${
+            !showMobileMenu ? "is-hidden-touch" : undefined
+          }`}
+        >
           {navItems.map((item) => (
-            <Link
-              className="navbar-item is-hidden-desktop"
-              to={item.link}
-              key={item.name}
-            >
-              {item.name}
-            </Link>
-          ))}
-          {socialMediaItems.map((item) => (
-            <a className="navbar-item" key={item.iconName} href={item.link} target="_blank" rel="noreferrer noopener">
-              <span className="icon is-large">
-                <i className={`fab ${item.iconName}`}></i>
-              </span>
-            </a>
-          ))}
-        </div>
-      </nav>
-    );
-
-    const navBarBottom = (
-      <nav
-        className="navbar is-hidden-touch"
-        role="navigation"
-        aria-label="main navigation"
-      >
-        <div className="navbar-brand"></div>
-        <div className="navbar-menu is-flex is-justify-content-space-around">
-          <div className="is-flex">
-            {navItems.map((item) => (
-              <Link className={`navbar-item ${item.name===props.name ? "is-active" : undefined}`} to={item.link} key={item.name}>
+            <>
+              <Link className="navbar-item" to={item.link} key={item.name}>
                 {item.name}
               </Link>
-            ))}
-          </div>
+              <span className="navitem-divider"></span>
+            </>
+          ))}
         </div>
       </nav>
     );
@@ -108,17 +87,31 @@ const Layout = (props) => {
       <footer class="footer">
         <div class="columns">
           <div class="column is-one-third">
-            <p className="has-text-centered">TODO: address</p>
+            <p className="has-text-centered">
+              TODO: address <br/>
+              {socialMediaItems.map((item) => (
+                <a
+                  key={item.iconName}
+                  href={item.link}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <span className="icon is-large">
+                    <i className={`fab ${item.iconName}`}></i>
+                  </span>
+                </a>
+              ))}
+            </p>
           </div>
           <div class="column is-one-third">
             <p className="has-text-centered">TODO: site search</p>
           </div>
           <div class="column is-one-third">
             <div className="container is-flex is-justify-content-center">
-            <GatsbyImage
-              alt="QMUL logo"
-              image={data.qmullogo.childImageSharp.gatsbyImageData}
-            />
+              <GatsbyImage
+                alt="QMUL logo"
+                image={data.qmullogo.childImageSharp.gatsbyImageData}
+              />
             </div>
           </div>
         </div>
@@ -127,15 +120,7 @@ const Layout = (props) => {
 
     return (
       <>
-        <header className="hero is-medium is-primary">
-          <div className="hero-head">{navBarTop}</div>
-          <div className="hero-body">
-            <p className="title is-size-1 has-text-centered">
-              Centre for Digital Music
-            </p>
-          </div>
-          <div className="hero-footer">{navBarBottom}</div>
-        </header>
+        {navBar}
         <main className="container">{props.children}</main>
         {footer}
       </>
