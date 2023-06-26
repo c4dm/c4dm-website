590b47a41b22e5417872def5fe6bc596721f530c
SFRL <sebastian.lobbers@googlemail.com>
2023-06-26 17:32:36 +0100

    create research cards dynamically

--- src/pages/research/index.js
+++ src/pages/research/index.js
@@ -1,162 +1,202 @@
 import React from "react";
-import {Link} from "gatsby";
+import {graphql,useStaticQuery,Link} from "gatsby";
 import Layout from "../../components/layout";
 import {StaticImage} from "gatsby-plugin-image";
 import ParallelogramHeader from "../../components/parallelogramHeader";
+import BlogCard from "../../components/blogCard";
 
 // THIS IS A HOT MESS!!!! THE CARDS SHOULD BE ABSTRACTED AS COMPONENTS AND IMAGES SHOULD BE IMPORTED THROUGH GRAPHQL AND NOT STATICALLY [Sebastian]
 
 // TODO: I think we should change this to dynamically create the links from gatsby nodes [ANH]
+
+
+
+
 const Research = ({pageContext}) => {
   const {
     breadcrumb: { crumbs },
   } = pageContext
+
+    const data = useStaticQuery(graphql`
+      {
+        pages: allFile(
+          filter: {
+            absolutePath: { regex: "/pages/research/" }
+            extension: { nin: "js" }
+          }
+        ) {
+          edges {
+            node {
+              id
+              name
+              childImageSharp {
+                gatsbyImageData(
+                  layout: CONSTRAINED
+                )
+              }
+            }
+          }
+        }
+      }
+    `);
     return (
         <Layout name="Research" crumbs={crumbs}>
             <section className="section ">
-            
-            <ParallelogramHeader text="Research" backgroundColor="primary" textColor="white"/>
+                <ParallelogramHeader text="Research" backgroundColor="primary" textColor="white"/>
 
-<div className="lowerPadding"> </div>
+                <div className="lowerPadding"></div>
            
-            <div className="columns is-flex">
-                        <div className="column is-one-third">
-                            <div className="card is-flex is-flex-direction-column is-flex-grow-1">
-                                <Link to="./labs">
-                                <div className="card-image is-256x256">
-                                    <figure className="image ">
-                                        <StaticImage
-                                            alt="default event picture as no event picture was specified"
-                                            src="../../images/groups.png"
-                                        />
-                                    </figure>
-                                </div>
-                                <div className="card-content">
-                                    <div className="media">
-                                        <div className="media-content">
-                                            <p className="title is-4">Research Labs </p>
-                                            
-                                        </div>
-                                    </div>
-                                    
-                                </div>
-                                </Link>
+                <div className="columns is-flex is-multiline">
+                    {
+                        data.pages.edges.map((page) => (
+                            <div className="column is-one-third">
+                                <BlogCard 
+                                    title={page.node.name} 
+                                    slug={`${page.node.name}`} 
+                                    image={page.node.childImageSharp.gatsbyImageData} 
+                                    largeTitle={true}
+                                />
                             </div>
-                        </div>
-                      
-
-                       
-                        <div className="column is-one-third is-flex">
-                            <div className="card is-flex is-flex-direction-column is-flex-grow-1">
-                                <Link to="./projects">
-                                <div className="card-image is-256x256">
-                                   
-                                    <figure className="image ">
-                                        
-                                        <StaticImage
-                                            alt="default event picture as no event picture was specified"
-                                            src="../../images/projects.png"
-                                        />
-                                    </figure>
-                                </div>
-
-                                <div className="card-content">
-                                    <div className="media">
-                                        <div className="media-content">
-                                            <p className="title is-4">Projects </p>
-                                            
-                                        </div>
-                                    </div>
-                                    
-                                </div>
-                                </Link>
-                            </div>
-                        </div>
-
-                        <div className="column is-one-third is-flex">
-                            <div className="card is-flex is-flex-direction-column is-flex-grow-1">
-                                <Link to="./publications">
-                                <div className="card-image is-256x256">
-                                    <figure className="image ">
-                                        
-                                        <StaticImage
-                                            alt="default event picture as no event picture was specified"
-                                            src="../../images/publications.png"
-                                        />
-                                    </figure>
-                                </div>
-
-                                <div className="card-content">
-                                    <div className="media">
-                                        <div className="media-content">
-                                            <p className="title is-4">Publications </p>
-                                            
-                                        </div>
-                                    </div>
-                                    
-                                </div>
-                                </Link>
-                            </div>
-                        </div>
+                        ))
+                    }
                 </div>
-                        <div className="columns is-flex">
-
-                        <div className="column is-one-third is-flex">
-                            <div className="card is-flex is-flex-direction-column is-flex-grow-1">
-                                <Link to="./datasets">
-                                <div className="card-image">
-                                    <figure className="image">
-                                        
-                                        <StaticImage
-                                            alt="default event picture as no event picture was specified"
-                                            src="../../images/code.png"
-                                        />
-                                    </figure>
-                                </div>
-
-                                <div className="card-content">
-                                    <div className="media">
-                                        <div className="media-content">
-                                            <p className="title is-4">Datasets & Code </p>
-                                            
-                                        </div>
-                                    </div>
-                                    
-                                </div>
-                                </Link>
-                            </div>
-                        </div>
-
-                        <div className="column is-one-third is-flex">
-                            <div className="card is-flex is-flex-direction-column is-flex-grow-1">
-                            <Link to="./patents">
-                                <div className="card-image">
-                                    <figure className="image">
-                                        
-                                        <StaticImage
-                                            alt="default event picture as no event picture was specified"
-                                            src="../../images/patents.png"                                        />
-                                    </figure>
-                                </div>
-
-                                <div className="card-content">
-                                    <div className="media">
-                                        <div className="media-content">
-                                            <p className="title is-4">Patents </p>
-                                            
-                                        </div>
-                                    </div>
-                                    
-                                </div>
-                                </Link>
-                            </div>
-                        </div>
-               
-           </div>
                 
-                </section>
+            </section>
         </Layout>
     );
 }
 
 export default Research;
+
+
+{/* <div className="column is-one-third">
+    <div className="card is-flex is-flex-direction-column is-flex-grow-1">
+        <Link to="./labs">
+        <div className="card-image is-256x256">
+            <figure className="image ">
+                <StaticImage
+                    alt="default event picture as no event picture was specified"
+                    src="../../images/groups.png"
+                />
+            </figure>
+        </div>
+        <div className="card-content">
+            <div className="media">
+                <div className="media-content">
+                    <p className="title is-4">Research Labs </p>
+                    
+                </div>
+            </div>
+            
+        </div>
+        </Link>
+    </div>
+</div>
+
+
+
+<div className="column is-one-third is-flex">
+    <div className="card is-flex is-flex-direction-column is-flex-grow-1">
+        <Link to="./projects">
+        <div className="card-image is-256x256">
+            
+            <figure className="image ">
+                
+                <StaticImage
+                    alt="default event picture as no event picture was specified"
+                    src="../../images/projects.png"
+                />
+            </figure>
+        </div>
+
+        <div className="card-content">
+            <div className="media">
+                <div className="media-content">
+                    <p className="title is-4">Projects </p>
+                    
+                </div>
+            </div>
+            
+        </div>
+        </Link>
+    </div>
+</div>
+
+<div className="column is-one-third is-flex">
+    <div className="card is-flex is-flex-direction-column is-flex-grow-1">
+        <Link to="./publications">
+        <div className="card-image is-256x256">
+            <figure className="image ">
+                
+                <StaticImage
+                    alt="default event picture as no event picture was specified"
+                    src="../../images/publications.png"
+                />
+            </figure>
+        </div>
+
+        <div className="card-content">
+            <div className="media">
+                <div className="media-content">
+                    <p className="title is-4">Publications </p>
+                    
+                </div>
+            </div>
+            
+        </div>
+        </Link>
+    </div>
+</div>
+</div>
+<div className="columns is-flex">
+
+<div className="column is-one-third is-flex">
+    <div className="card is-flex is-flex-direction-column is-flex-grow-1">
+        <Link to="./datasets">
+        <div className="card-image">
+            <figure className="image">
+                
+                <StaticImage
+                    alt="default event picture as no event picture was specified"
+                    src="../../images/code.png"
+                />
+            </figure>
+        </div>
+
+        <div className="card-content">
+            <div className="media">
+                <div className="media-content">
+                    <p className="title is-4">Datasets & Code </p>
+                    
+                </div>
+            </div>
+            
+        </div>
+        </Link>
+    </div>
+</div>
+
+<div className="column is-one-third is-flex">
+    <div className="card is-flex is-flex-direction-column is-flex-grow-1">
+    <Link to="./patents">
+        <div className="card-image">
+            <figure className="image">
+                
+                <StaticImage
+                    alt="default event picture as no event picture was specified"
+                    src="../../images/patents.png"                                        />
+            </figure>
+        </div>
+
+        <div className="card-content">
+            <div className="media">
+                <div className="media-content">
+                    <p className="title is-4">Patents </p>
+                    
+                </div>
+            </div>
+            
+        </div>
+        </Link>
+    </div>
+</div> */}
