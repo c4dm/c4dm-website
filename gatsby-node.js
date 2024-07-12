c00a93ba603f59cd8d21a8a274fcf484a800cb66
Christos Plachouras <cplachouras@gmail.com>
2024-07-12 12:58:06 +0300

    redirect archive pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -112,14 +112,35 @@ exports.createPages = async ({ graphql, actions }) => {
         })
     });
 
-    // Define Redirects
+    // Define archive redirects
+    const archivePages = [
+        'auda-hpss', 'analysis', 'bssica', 'CBFdataset', 'ChoralSep', 'coding', 'education', 'engagement', 'events',
+        'fellowships', 'm4m', 'people_past', 'phds', 'PhDStudentships', 'phdstudy', 'platform', 'processing',
+        'projects_old', 'ProposedPhDTopics', 'seminars-historical', 'seminars', 'software_data', 'soundsynthesis',
+        'study', 'WANCM_keynotes', 'WANCM_programme', 'WANCM', 'a-maps', 'ensembleset', 'horse2016', 'horse2017',
+        'icassp18', 'ismir15-amt-tutorial', 'ismir17', 'ismir19', 'nus-amt-tutorial', 'sceneseventschallenge',
+        'taslp19', 'taslp20', 'WASPAA19-HPSS'
+    ]; 
+
+    // Create redirects for archive pages
+    archivePages.forEach((pageName) => {
+        createRedirect({
+            fromPath: `/${pageName}`,
+            toPath: `/archive/${pageName}`,
+            isPermanent: true,
+            redirectInBrowser: true,
+            statusCode: 301,
+        });
+    });
+
+    // Define legacy redirects
     const legacyServer = 'http://c4dm-legacy.eecs.qmul.ac.uk'
 
     // Add more paths here for redirects
     // const legacyPages = ['smdmrd-test', 'rdr', 'semanticmedia', 'dmrn', 'soundscape', 'wiki', 'multitrack', 'dead', 'mirrors/omras2.org', 'preceedings', 'research/worldmusicoutliers', 'shapetones', 'datasets', '20thanniversary', 'auda-hpss', 'EnsembleSet']
     const legacyPages = ['smdmrd-test', 'rdr', 'semanticmedia', 'dmrn', 'soundscape', 'wiki', 'multitrack', 'dead', 'mirrors/omras2.org', 'preceedings', 'research/worldmusicoutliers', 'shapetones', 'datasets']
 
-    // Create Redirects
+    // Create legacy redirects
     legacyPages.forEach((pageName) => {
         createRedirect({ fromPath: pageName, toPath: (legacyServer+"/"+pageName), statusCode: 200, isPermanent: true });
         // Handle wildcards
